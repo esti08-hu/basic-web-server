f498dbcc51afced69d81c8e530d7a7d44a9ad6e1
esti08-hu <estioame@gmail.com>
2024-11-16 17:43:30 +0300

    added httpcode for the routes

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, HttpCode } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
@@ -6,16 +6,19 @@ export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get('/name')
+  @HttpCode(200)
   getName(): string {
     return this.appService.name();
   }
 
   @Get('/hobby')
+  @HttpCode(200)
   getHobby(): string {
     return this.appService.hobby();
   }
 
   @Get('/dream')
+  @HttpCode(200)
   getDream(): string {
     return this.appService.dream();
   }
